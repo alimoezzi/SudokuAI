d4b7b6172abaf45f35ce6f0401929cb0769dc253
s_arme <s.arme@ya.ru>
2020-02-02 23:49:41 +0330

    add display method in utils

---
+++ utils.py
@@ -0,0 +1,13 @@
+def display(values, rows, cols, boxes):
+    """
+    Display the values as a 2-D grid.
+    Input: The sudoku in dictionary form
+    Output: None
+    """
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
